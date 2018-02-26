17bd536e6a72669f4f132c9fe58629b05746f226
MrBobinsky <5yx4r@mail.ru>
2018-02-26 12:45:53 +0300

    github

--- Program.cs
+++ Program.cs
@@ -13,7 +13,10 @@ namespace TypeConversions
             Console.WriteLine("********* Fun with type conversions **********");
             //добавить две переменные типа short и вывести результат.
             short numb1 = 9, numb2 = 10;
-            Console.WriteLine();
+            Console.WriteLine("{0}+{1}={2}", numb1, numb2, Add(numb1, numb2));
+            Console.ReadLine();
+            
         }
+
     }
 }
